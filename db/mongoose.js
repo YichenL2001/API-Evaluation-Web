91eedaee9cb9197343c0d7147974c216e885bff1
liuyi104 <krystal.liu@mail.utoronto.ca>
2021-12-07 13:02:20 -0500

    fix 4

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || "mongodb+srv://csc309:csc309@cluster0.bew2t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoURI = process.env.MONGODB_URI || "mongodb://localhost:27017/StudentAPI";
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
